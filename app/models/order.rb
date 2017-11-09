06cb0ee2ca349e561dd4247cd5d6f49eda6273cf
makun <michaelmakun@aliyun.com>
2017-11-09 23:35:16 +0800

    add product_list model and order controller

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,2 +1,10 @@
 class Order < ApplicationRecord
+  belongs_to :user
+
+  validates_presence_of :billing_name
+  validates_presence_of :billing_address
+  validates_presence_of :shipping_name
+  validates_presence_of :shipping_address
+
+  has_many :product_lists
 end
